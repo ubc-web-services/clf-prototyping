d0f8f0aeabd15c3d70390df70201b0c12c85fee1
James <james.otoole@ubc.ca>
2015-09-14 10:41:47 -0700

    update js

--- js/custom.js
+++ js/custom.js
@@ -1,62 +1,57 @@
 // Custom Javascript if necessary
 (function($) {
-// Insert necessary javascript stuff here
 
-/*var cssLink = document.createElement("link") 
-cssLink.href = "style.css"; 
-cssLink .rel = "stylesheet"; 
-cssLink .type = "text/css"; 
-frames['wayfinder'].document.body.appendChild(cssLink);*/
-
-
-
-$(document).ready(function() {
-	// ... or here
-
-	// tabs
-	$('.tabs a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
+  $(document).ready(function() {
+    
+    // !!! remove from production !!!
+  	// tabs
+  	$('.tabs a').click(function (e) {
+      e.preventDefault();
+      $(this).tab('show');
+    });
+    
+    
+    // !!! remove from production !!!
+    // accordion
+    $('.accordion').find('.accordion-tab').click(function(e){
+      e.preventDefault();
+      //Expand or collapse this panel
+      $(this).next().slideToggle('fast');
+      $(this).parent().toggleClass('accordion-closed');
+      //$(this).find(">:first-child").toggleClass('icon-plus-sign icon-minus-sign');
+      //Hide the other panels
+      //$(".accordion-content").not($(this).next()).slideUp('fast');
+    });
+    
+    
+    
+    /* FORMSTONE - form element changes */
+    $("input[type=checkbox], input[type=radio]").checkbox();
+    $("select").dropdown();
+    $(".equalize").equalize({
+        target: ".btn"
+    });
+    $(".row.profiles").equalize();
+    
+    
+    // add smooth scroll to anchors
+    $('a.smoothscroll[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') || location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+               if (target.length) {
+                 $('html,body').animate({
+                     scrollTop: target.offset().top
+                }, 600);
+                return false;
+            }
+        }
+    });  
   });
-  
-  // accordian
-  $('.panel').find('.panel-tab').click(function(e){
-    e.preventDefault();
-    //Expand or collapse this panel
-    $(this).next().slideToggle('fast');
-    $(this).parent().toggleClass('panel-closed panel-open');
-    $(this).find(">:first-child").toggleClass('icon-plus-sign icon-minus-sign');
-    //Hide the other panels
-    //$(".accordion-content").not($(this).next()).slideUp('fast');
-  });
-  
-  // accordion open / close button
-  $('#openall').click(function(e){
-    e.preventDefault();
-    //Expand or collapse this panel
-    $('.panel-content').slideDown('fast');
-    $('.panel-tab > i').removeClass('icon-plus-sign').addClass('icon-minus-sign');
-    $('.panel').removeClass('panel-closed').addClass('panel-open');
-  });
-  $('#closeall').click(function(e){
-    e.preventDefault();
-    //Expand or collapse this panel
-    $('.panel-content').slideUp('fast');
-    $('.panel-tab > i').removeClass('icon-minus-sign').addClass('icon-plus-sign');
-    $('.panel').removeClass('panel-open').addClass('panel-closed');
-  });
-  
-});
 
-$(window).load(function() {
-	
-	// front page carousel
-	$('.carousel').ubc_spotlight({
-    interval: 6000,
-    pause: 'hover'
-	});
-	$('.flexslider').flexslider();
-	
- });
+  $(window).load(function() {
+    // initialize flexslider slideshows
+    $('.flexslider').flexslider();
+  });
 
 })(jQuery);
